3fd8495b174c0ae722919d21e1c5690b9c8a8a46
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-09 15:45:57 +0800

    Update _RUN.bat

--- _RUN.bat
+++ _RUN.bat
@@ -1,3 +1,14 @@
 @echo off
-python _EDIT.py
+:: create env
+echo # Creating environment
+python -m venv .venv
+:: use env
+call .\.venv\Scripts\activate.bat
+:: install libr
+echo # Installing library needed
+python -m pip install -r reqlibrary.txt
+:: clear screen
+cls
+:: run web
+python -m streamlit run index.py
 pause
