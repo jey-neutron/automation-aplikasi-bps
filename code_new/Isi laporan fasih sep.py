ea3d147698645bcf643a492b4ace86249537392d
Jimmy Nickelson <64954344+jey-neutron@users.noreply.github.com>
2024-06-09 22:05:27 +0800

    Update Isi laporan fasih sep.py

--- code_new/Isi laporan fasih sep.py
+++ code_new/Isi laporan fasih sep.py
@@ -39,10 +39,29 @@ def cek_input(namafile='temp_input.txt'):
 # for main view func
 def desc():
     # desc memuat keterangan program, sso buat apa, nama survei di fasih, dataframe used, rentang baris
-    return "1) Program untuk ngisi laporan SEP di Fasih-sm \n 2) SSO untuk login Fasih \n 3) Nama survei isiin SURVEI PELAPORAN LAPANGAN SEP 2024 \n 4) CSV gunakan assign_cawi, tapi harus diedit tiap saat yah sebelum run program dan nama sheetnya samain nama file \n 5) Rentang baris menandakan kode kec. Amannya satu2 karna Fasihnya perlu kode antirobot"
+    return "1) Program untuk ngisi laporan SEP di Fasih-sm \n 2) SSO untuk login Fasih \n 3) Nama survei isiin SURVEI PELAPORAN LAPANGAN SEP 2024 \n 4) CSV gunakan assign_cawi, tapi harus diedit tiap saat yah sebelum run program dan nama sheetnya samain nama file \n Kolom df: 'kec' adalah kode kec dalam int, 'date' adalah date now dengan format 'dd mm yyyy', 'status' kode status dalam int, 'utp_selesai' jml int utp selesai \n 5) Rentang baris menandakan kode kec. Amannya satu2 karna Fasihnya perlu kode antirobot"
 
 def RUN(ssoname, ssopass, pilihan_survei, df_name, rentang):
     try:
+        # cek df dulu
+        columns_wajib = ['kec','date','status','utp_selesai']
+        logger.info(f'# Data yang mau dipake adalah {df_name}. Kolom wajib ada: {columns_wajib} ')
+        # read df yang terpilih
+        logger.info("# Checking df")
+        df = pd.read_excel(str(this_path)+"/"+df_name, sheet_name=df_name.split(".")[0])
+        columns_df = [x for x in columns_wajib if x in df.columns]
+        logger.info(df.head())
+        ## cek kolom data csv
+        if columns_df != columns_wajib: 
+            kataerror = '# Maaf data tidak memenuhi kriteria. \nKolom wajib ada: '+str(columns_wajib)+\
+            '\nData anda: '+str([i for i in df.columns])+\
+            '\nSilakan cek data kembali'
+            logger.info("WARN: "+kataerror)
+            notif.show_toast("Assign fasih PY", "ERROR HAPPEN :(", duration = 1)
+            raise Exception(kataerror)
+            #exit
+        
+        
         # Open mozilla
         logger.info("# Opening mozilla ")
         # add profile extension automa
@@ -91,11 +110,6 @@ def RUN(ssoname, ssopass, pilihan_survei, df_name, rentang):
         notif = MyToastNotifier()
         #notif.show_toast("Assign fasih PY", "YEYYY SELESE :)", duration = 1)
 
-        # read df
-        logger.info("WARN: Cek dulu df nya")
-        df = pd.read_excel(str(this_path)+"/"+df_name, sheet_name=df_name.split(".")[0])
-        logger.info(df.head())
-        
         # ALL run
         ikec = rentang
         if "-" in str(ikec):
